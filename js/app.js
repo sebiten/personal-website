b78ac145dc53aef28359ccd63ef8b0056b0900ac
Sergio Sebastian burgos <sebamayorde18@gmail.com>
2022-09-12 15:06:37 -0300

    finishing <3

--- js/app.js
+++ js/app.js
@@ -1,52 +1,54 @@
-document.addEventListener('DOMContentLoaded', function(){
-    iniciarApp();
-  
+document.addEventListener("DOMContentLoaded", function () {
+  iniciarApp();
 });
 
 function iniciarApp() {
-    scrollNav();
-  
+  scrollNav();
 }
 function scrollNav() {
-    const enlaces = document.querySelector('.about-nav a');
-    enlaces.addEventListener('click', function(e){
-        e.preventDefault();
-        const seccionScroll = e.target.attributes.href.value;
-        const seccion = document.querySelector(seccionScroll)
-        seccion.scrollIntoView({behavior: 'smooth'});
-    });
-    const enlaces1 = document.querySelector('.sidebar .menu .one');
-    enlaces1.addEventListener('click', function(e) {
-        e.preventDefault();
-        const seccionScroll = e.target.attributes.href.value;
-        const seccion = document.querySelector(seccionScroll)
-        seccion.scrollIntoView({behavior: 'smooth'})
-    })
-    const enlaces3 = document.querySelector('.sidebar .menu .three');
-    enlaces3.addEventListener('click', function(e) {
-        e.preventDefault();
-        const seccionScroll = e.target.attributes.href.value;
-        const seccion = document.querySelector(seccionScroll)
-        seccion.scrollIntoView({behavior: 'smooth'})
-    })
-    const enlaces4 = document.querySelector('.sidebar .menu .four');
-    enlaces4.addEventListener('click', function(e) {
-        e.preventDefault();
-        const seccionScroll = e.target.attributes.href.value;
-        const seccion = document.querySelector(seccionScroll)
-        seccion.scrollIntoView({behavior: 'smooth'})
-    })
+  const enlaces = document.querySelector(".about-nav a");
+  enlaces.addEventListener("click", function (e) {
+    e.preventDefault();
+    const seccionScroll = e.target.attributes.href.value;
+    const seccion = document.querySelector(seccionScroll);
+    seccion.scrollIntoView({ behavior: "smooth" });
+  });
+  const enlaces1 = document.querySelector(".sidebar .menu .one");
+  enlaces1.addEventListener("click", function (e) {
+    e.preventDefault();
+    const seccionScroll = e.target.attributes.href.value;
+    const seccion = document.querySelector(seccionScroll);
+    seccion.scrollIntoView({ behavior: "smooth" });
+  });
+  const enlaces3 = document.querySelector(".sidebar .menu .three");
+  enlaces3.addEventListener("click", function (e) {
+    e.preventDefault();
+    const seccionScroll = e.target.attributes.href.value;
+    const seccion = document.querySelector(seccionScroll);
+    seccion.scrollIntoView({ behavior: "smooth" });
+  });
+  const enlaces4 = document.querySelector(".sidebar .menu .four");
+  enlaces4.addEventListener("click", function (e) {
+    e.preventDefault();
+    const seccionScroll = e.target.attributes.href.value;
+    const seccion = document.querySelector(seccionScroll);
+    seccion.scrollIntoView({ behavior: "smooth" });
+  });
 }
 
+const $form = document.querySelector("#form");
+const $buttonMailto = document.querySelector("#trucazo");
 
-const $form = document.querySelector('#form')
-const $buttonMailto = document.querySelector('#trucazo')
-
-$form.addEventListener('submit', handleSubmit)
+$form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
-  event.preventDefault()
-  const form = new FormData(this)
-  $buttonMailto.setAttribute('href', `mailto:sebdevspace@gmail.com.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
-  $buttonMailto.click()
+  event.preventDefault();
+  const form = new FormData(this);
+  $buttonMailto.setAttribute(
+    "href",
+    `mailto:sebdevspace@gmail.com.com?subject=nombre ${form.get(
+      "name"
+    )}  correo ${form.get("email")}&body=${form.get("message")}`
+  );
+  $buttonMailto.click();
 }
