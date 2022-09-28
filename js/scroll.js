68293cffc5a198746f0f9670e70c084d5a89b1c4
Sergio Sebastian burgos <sebamayorde18@gmail.com>
2022-09-28 15:56:26 -0300

    colo changes fin

---
+++ js/scroll.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    iniciarApp();
+  });
+  
+  function iniciarApp() {
+    scrollNav();
+    navegacionLink();
+    submit();
+  }
+  function scrollNav() {
+    const enlaces = document.querySelector(".about-nav a");
+    enlaces.addEventListener("click", function (e) {
+      e.preventDefault();
+      const seccionScroll = e.target.attributes.href.value;
+      const seccion = document.querySelector(seccionScroll);
+      seccion.scrollIntoView({ behavior: "smooth" });
+    });
+  }
+  function navegacionLink() {
+    const navegacion = document.querySelectorAll(".sidebar .menu a");
+    navegacion.forEach((navegacion) =>
+      navegacion.addEventListener("click", function (e) {
+        e.preventDefault();
+        const seccionScroll = e.target.attributes.href.value;
+        const seccion = document.querySelector(seccionScroll);
+        seccion.scrollIntoView({ behavior: "smooth" });
+      })
+    );
+  }
